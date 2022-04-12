beda9516dacb98e97689facd510d0ba1a1ce38fe
tsjazil <tsjazil@gmail.com>
2022-04-12 19:48:30 +0530

    Added comments

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
-require('plugins')
-require('keymaps')
-require('configs.nordic')
-require('options')
-require('configs.nvimtree')
-require('configs.bufferline')
-require('configs.lualine')
-require('configs.comment')
-require('configs.indent')
-require('configs.treesitter')
+require('plugins') -- Plugins managed by packer
+require('keymaps') -- Keymaps for my workflow 
+require('configs.nordic') -- Colorscheme config
+require('options') -- vim.opts and some highlighting 
+require('configs.nvimtree') -- configs for nvim_tree
+require('configs.bufferline') -- bufferline config
+require('configs.lualine') -- lualine config
+require('configs.comment') --- nvim.comment config
+require('configs.indent') -- indentation config
+require('configs.treesitter') -- treesitter config
